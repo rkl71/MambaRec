97f8fae9fa01c62dd80fd84995b80418ad821018
rkl71 <renkelin00@gmail.com>
2025-05-22 18:47:40 +0900

    init project

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ os.environ['NUMEXPR_MAX_THREADS'] = '48'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='MGCN', help='name of models')
+    parser.add_argument('--model', '-m', type=str, default='MambaRec', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='baby', help='name of datasets')
 
     config_dict = {
